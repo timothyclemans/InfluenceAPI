43b0fd06d604d2870d7390c405e339b8c17c6580
Tim Clemans <timothy.clemans@gmail.com>
2013-05-21 15:37:15 -0700

    added homepage with link to harshstartup_converter and added link to edit patterns and replacements

--- influenceapi/urls.py
+++ influenceapi/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -15,6 +16,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    (r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^decisions/$', 'decisiontrees.views.decisions'),
     url(r'^decisions/(?P<id>\d+)/$', 'decisiontrees.views.decisions'),
