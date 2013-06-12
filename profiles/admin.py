3efd75d7d15d40cf23c9a9a1f9b4e379a0cffc35
Tom McDonald <tom@volturius.com>
2013-06-11 17:43:31 -0700

    Created model for word replacement

---
+++ profiles/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from models import WordReplacement
+
+admin.site.register(WordReplacement)
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class WordReplacement(models.Model):
+
+    word        = models.CharField(max_length = 15)
+    replacement = models.CharField(max_length = 15)
+
+    def __unicode__(self):
+        return '%s:%s' % (self.word, self.replacement)
